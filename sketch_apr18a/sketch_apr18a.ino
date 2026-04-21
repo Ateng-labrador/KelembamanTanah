9869c10b62e25c10e3597fb00d1e48dda48c0a3f
Ateng-labrador <arlantengga8@gmail.com>
2026-04-21 14:21:45 +0700

    feat:first commit

---
+++ sketch_apr18a/sketch_apr18a.ino
@@ -0,0 +1,17 @@
+const int SoilSensor = A0;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float kelembabanTanah;
+  int hasilPembacaan = analogRead(SoilSensor);
+  kelembabanTanah = (100 - ((hasilPembacaan/1021.00)*100));
+
+  Serial.print("Persentase Kelembaban Tanah = ");
+  Serial.print(kelembabanTanah);
+  Serial.println("%");
+  delay(1000);
+}
